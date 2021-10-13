cda79eb69e152ebcd5669bff351e4ea8a9fed0dd
vegAs-RW <jordan.pouchot2@gmail.com>
2021-10-13 16:41:12 +0200

    modification des requetes post et get

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Sauce = require('./models/sauces');
+
+mongoose.connect('mongodb+srv://jordan-dev:Joelpocho11@cluster0.pdzno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+ 
+app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.post('/api/sauces', (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body
+    });
+    thing.save()
+    .then(() => res.status(201).json({ message : 'Nouvelle sauce enregistrée'}))
+    .catch(error => res.status(400).json ({ error}));
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+
 
 module.exports = app;
