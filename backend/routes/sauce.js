93c11ade2966edb89935aab485111c1ffc225ce0
vegAs-RW <jordan.pouchot2@gmail.com>
2021-10-14 09:28:06 +0200

    separation de la logique metier, de la logique de routing

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,41 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const Sauce = require('../models/sauces');
+const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-    .then(() => res.status(201).json({ message : 'Nouvelle sauce enregistrée'}))
-    .catch(error => res.status(400).json ({ error}));
-});
-
-router.put('/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-router.delete('/:id', (req, res, next) => {
-    Sauce.deleteOne({_id: req.params.id})
-    .then(() => res.status(200).json({message: 'Sauce supprimée'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-router.get('/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-});
-
-router.get('/', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
+router.post('/', sauceCtrl.createSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/', sauceCtrl.getAllSauces);
 
 
 module.exports = router;
