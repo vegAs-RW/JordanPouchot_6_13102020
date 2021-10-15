a495999bbadb6f4da973edd3c629326f98f53055
vegAs-RW <jordan.pouchot2@gmail.com>
2021-10-15 12:23:52 +0200

    ajout de la routes pour les likes/dislikes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceCtrl.createSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce)
 
 module.exports = router;
